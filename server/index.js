4f9c34e35f4b72ca23294a4763ba2221fbd36cfd
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-19 01:06:26 -0500

    dummy data

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8080;
 
 // app
+const books = [
+  { name: "nice", author: "me" },
+  { name: "dope", author: "also me" },
+]
+
 const app = express();
 
 app.get('/test', (req, res) => {
   res.send('She works!')
+  res.json(books)
 })
 
 
-// port setup
-const port = process.env.PORT || 8080;
+// run
 app.listen(port);
 
 console.log(`currently listening on port ${port}.`);
