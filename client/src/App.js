a3057a5772cd99f2a882909b9bc7aa98dc9b66c8
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-18 19:54:44 -0500

    added axios dependency, connected server to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+const hitServer = () => {
+  axios.get('/test')
+  .then((res) => {
+    console.log(res.data);
+  });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={hitServer}>Send Request</button>
     </div>
   );
 }
